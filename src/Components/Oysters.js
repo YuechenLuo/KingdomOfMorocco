10094616b2f2da59595962b66d6635a29ad304e5
Yuechen Luo <yuechen4c@gmail.com>
2019-03-13 16:50:56 -0700

    Added message

--- src/Components/Oysters.js
+++ src/Components/Oysters.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import HJ from '../Assets/img/hj.png';
+import '../css/oyster.css';
 
 class Oysters extends Component {
 
@@ -10,11 +12,18 @@ class Oysters extends Component {
         return "";
     }
     return <div className="oyster-body">
-        <p>听说。。有些人想吃生蚝？？</p>
-        <a>点我点我</a>
-        <p>5个优惠码</p>
-
-        <p>可以带林那啥一起吃</p>
+        <img src={HJ} alt="lalala"/>
+        <div className="content">
+            <p>听说。。有些人想吃生蚝？？</p>
+            <a>点我点我</a>
+            <p>5个优惠码</p>
+            <p className="promo-code">
+                1234512345123451234512345<br/>
+                12345<br/>
+                12345<br/>
+            </p>
+            <p>可以带林那啥一起吃</p>
+        </div>
     </div>;
   }
 }
