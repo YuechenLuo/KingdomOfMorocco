c08d618c0141ee35d09cb6424da4cf19dc88a44f
Yuechen <yuechen4c@gmail.com>
2019-03-26 21:31:18 -0700

    update Dozo content

--- src/Components/Dozo.js
+++ src/Components/Dozo.js
@@ -12,14 +12,14 @@ class Dozo extends Component {
     }
     return <div className="oyster-body">
             <p>找到工作请你们吃饭😝</p>
-            <a href="https://www.meituan.com/meishi/d44423680.html">优惠券信息</a><br/>
+            <a href="https://www.meituan.com/meishi/d44423680.html">>>>点击查看优惠券信息</a><br/>
             <a href="http://www.meituan.com/meishi/70863/">家门口环亚广场这家店</a>
-            <p>4个优惠码, 带公公舅舅一起吃</p>
+            <p>4个优惠码, 一个优惠码一个人，带公公舅舅一起吃</p>
             <p className="promo-code">  
-              美团券密码：101381180203 未消费
-              美团券密码：019049180749 未消费
-              美团券密码：124595480659 未消费
-              美团券密码：098701880335 未消费
+              美团券密码：101381180203 未消费<br/>
+              美团券密码：019049180749 未消费<br/>
+              美团券密码：124595480659 未消费<br/>
+              美团券密码：098701880335 未消费<br/>
             </p>
     </div>;
   }
